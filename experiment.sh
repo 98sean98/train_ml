f0b2c881f4dfdd36b0d4b5d6a68c5fd90ac31c78
Sean Chok <seanchok@gmail.com>
2022-04-28 21:33:34 +0800

    simplify experiment.sh

--- experiment.sh
+++ experiment.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo "start experiment run!"
+python --version
 
-echo "data directory: $USER_EXPERIMENT_DATA_DIRECTORY"
-echo "artifacts directory: $USER_EXPERIMENT_ARTIFACTS_PATH"
-
-ls $USER_EXPERIMENT_DATA_DIRECTORY
-
-python $USER_EXPERIMENT_DIRECTORY/train.py
-cat $USER_EXPERIMENT_ARTIFACTS_PATH/output.txt
-
-echo "end experiment run!"
+python train.py
